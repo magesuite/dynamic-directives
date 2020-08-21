68dca1c37c2dbd62224a7e0d39bc8e72e7d0fe1e
Krzysztof Kurzydło <krzysztof.kurzydlo@creativestyle.pl>
2020-08-21 16:38:36 +0200

    Compatibility with PHPUNIT 9

--- Test/Unit/Service/DirectiveApplierTest.php
+++ Test/Unit/Service/DirectiveApplierTest.php
@@ -19,7 +19,7 @@ class DirectiveApplierTest extends \PHPUnit\Framework\TestCase
      */
     protected $directiveApplier;
 
-    public function setUp()
+    public function setUp(): void
     {
         $this->objectManager = \Magento\TestFramework\ObjectManager::getInstance();
 
